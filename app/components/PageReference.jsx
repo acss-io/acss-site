3e02c39e7c632d93e0c812a0d643f332b6863a6a
Thierry Koblentz <thierry.koblentz@gmail.com>
2016-01-31 15:12:24 -0800

    Add paragraph about ATOMIZER WEB
    ATOMIZER WEB is the online tool built by Pankaj

--- app/components/PageReference.jsx
+++ app/components/PageReference.jsx
@@ -15,6 +15,7 @@ class PageReference extends React.Component {
             <div id="reference" role="main" className="reference-page innerwrapper Mb(50px) Mx(10px) Maw(1000px)--sm Mx(a)--sm W(90%)--sm">
                 <h1>Reference</h1>
                 <p>Use this page to search for Atomic classes. Visit <a href="/guides/syntax.html" aria-label="about the syntax">this one</a> to understand the syntax, <a href="/guides/syntax.html#examples-" aria-label="examples">this one</a> to look at examples, <a href="/guides/atomic-classes.html" aria-label="about custom classes">this one</a> to learn about custom classes, and check <a href="/guides/helper-classes.html" aria-label="about the helpers">this one</a> for helper/utility classes.</p>
+                <p>You may also want to check <a href="https://pankajparashar.com/atomizer-web/">ATOMIZER WEB</a> (an online tool built by <a href="https://twitter.com/pankajparashar" title="@pankajparashar on Twitter">Pankaj Parashar</a>) which is a playground for ACSS. Add some Atomic classes and ATOMIZER WEB will show you the rendered CSS. The tool even gives you access to a config file where you can set your own break-points, variables, and more.</p>
                 <Reference />
             </div>
         );
